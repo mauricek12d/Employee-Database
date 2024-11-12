22e880d731dfc5a16354a1d0c68548830d081055
Maurice Zuniga <mauricek12d@gmail.com>
2024-11-11 23:16:57 -0500

    Made changes to the query which was showing the wrong manager_id information

--- db/schema.sql
+++ db/schema.sql
@@ -5,15 +5,14 @@ CREATE DATABASE employee_db;
 
 CREATE TABLE Department (
     id SERIAL PRIMARY KEY,
-    employee_name VARCHAR(30) UNIQUE NOT NULL
+    department_name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE Role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL NOT NULL;
-    department_id INTEGER NOT NULL;
-    FOREIGN KEY (department_id) REFERENCES Department (id)
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
 );
 
 CREATE TABLE Employee (
@@ -21,6 +20,5 @@ CREATE TABLE Employee (
     first_name VARCHAR(30)NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES Role (id),
+    manager_id INTEGER
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,13 @@
-INSERT INTO Department (employee_name) 
-VALUES ('Engineering'),
+INSERT INTO Department (department_name) 
+VALUES 
+('Engineering'),
 ('Sales'), 
 ('Finance'), 
 ('Legal');
 
 INSERT INTO Role (department_id, title, salary)
-VALUES (1, 'Lead Engineer', 100000),
+VALUES 
+(1, 'Lead Engineer', 100000),
 (1, 'Senior Engineer', 120000),
 (1, 'Account Manager', 150000),
 (2, 'Salesperson', 80000),
@@ -16,7 +18,8 @@ VALUES (1, 'Lead Engineer', 100000),
 (4, 'Lawyer', 190000);
 
 INSERT INTO Employee (role_id, first_name, last_name, manager_id) 
-VALUES (1, 'Jim', 'Hannon', NULL),
+VALUES 
+(1, 'Jim', 'Hannon', NULL),
 (2, 'Jane', 'Smith', 1),
 (3, 'David', 'Taylor', 2),
 (4, 'Ken', 'Cunningham', 2),
